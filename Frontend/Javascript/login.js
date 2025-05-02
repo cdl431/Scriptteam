bf86bf5537eeaf9ae63960e7306f15ff4d0ac366
Tnguyen0525 <71985660+Tnguyen0525@users.noreply.github.com>
2025-05-01 20:39:38 -0500

    deleted old frontend folder

--- Frontend/Javascript/login.js
+++
@@ -1,8 +0,0 @@
-import { loginUser } from "./auth.js";
-// replace with fetch when backend is ready
-const fakeUser = {
-  name: "Thomas Nguyen",
-  email: "thomas@ezwatch.com"
-};
-
-loginUser(fakeUser);
